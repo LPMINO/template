7ec871d45671dec51c64cf2d5740f1d3732556a9
LPMINO <frog1227@naver.com>
2020-11-25 18:12:01 +0900

    script update

--- script.js
+++ script.js
@@ -52,8 +52,8 @@ async function predict() {
     const probability = prediction[i].probability.toFixed(2) * 100;
 
     const data = {
-      Me: 'Me에 대한 설명을 적어볼까요?',
-      None: 'None에 대한 설명을 적어볼까요?',
+      Phone: 'Phone',
+      None: 'None',
     };
 
     if (probability >= 75) {
